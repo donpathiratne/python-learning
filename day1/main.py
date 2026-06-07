a644a6136001d853f57efc6cc37358b69f6db107
Yehan Pathiratne <yehanpathirathne@gmail.com>
2026-06-07 21:59:22 +0530

    Create main.py

---
+++ day1/main.py
@@ -0,0 +1,33 @@
+'''
+# this is my first python program.
+print("I like rolls.");
+print("It's really good.");
+
+
+# variables = A container for a value (String, integer, float, boolean)
+# A variable behaves as if it was the value it contains
+
+# Strings
+first_name = "Yehan" 
+food = "Rolls"
+email = "yehan@gmail.com"
+
+print(f"hello {first_name}")
+print(f"You like {food}.");
+print(f"Your email is {email}");
+
+
+# Integers
+age = 20
+quantity = 3
+number_if_students = 25
+
+print(f"You are {age} years old.")
+print(f"You are buying {quantity} items.")
+print(f"Your class has {number_if_students} students.")
+
+'''
+
+#Float
+price = 10.99
+print(f"The price is ${price}.")
