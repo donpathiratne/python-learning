aef7e5291963996aa7e01e618cb3521a1894cbf8
Yehan Pathiratne <yehanpathirathne@gmail.com>
2026-06-08 15:36:19 +0530

    Update variables part2

--- day1/main.py
+++ day1/main.py
@@ -30,4 +30,31 @@ print(f"Your class has {number_if_students} students.")
 
 #Float
 price = 10.99
+gpa = 3.2
+distance = 3.0
+
 print(f"The price is ${price}.")
+print(f"My GPA is {gpa}")
+print(f"You ran {distance}km.")
+
+# Boolean
+is_student = True
+#print(f"Are you a student? {is_student}")
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
+
+for_sale = True
+
+if for_sale:
+    print("This is for sale")
+else:
+    print("This is not for sale.")
+
+is_online = False
+
+if is_online:
+    print("You are online")
+else:
+    print("You are offline")
