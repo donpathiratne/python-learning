1d569519d1dc8f747d94e3da1187a9f1df6e8a9a
Yehan Pathiratne <yehanpathirathne@gmail.com>
2026-07-24 09:19:28 +0530

    Add lower() method
    Convert all user inputs to lowercase

--- quiz_game.py
+++ quiz_game.py
@@ -2,30 +2,30 @@ print("Welcome to my compuetr quiz!")
 
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":    # to lowercase all the letters
     quit()
 
 print("Okay! Let's Play")
 
-answer = input("What does CPU stand for? ")
+answer = input("What does CPU stand for? ").lower()
 if answer == "central processing unit":
     print("correct")
 else:
     print("Wrong")
 
-answer = input("What does GPU stand for? ")
+answer = input("What does GPU stand for? ").lower()
 if answer == "graphics processing unit":
     print("correct")
 else:
     print("Wrong")
 
-answer = input("What does RAM stand for? ")
+answer = input("What does RAM stand for? ").lower()
 if answer == "random access memory":
     print("correct")
 else:
     print("Wrong")
 
-answer = input("What does PSU stand for? ")
+answer = input("What does PSU stand for? ").lower()
 if answer == "power supply unit":
     print("correct")
 else:
