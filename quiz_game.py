06c2a45e21d01f308b2a7c4f7c7ce3a35ed8d804
Yehan Pathiratne <yehanpathirathne@gmail.com>
2026-07-24 09:24:00 +0530

    Add counting score part

--- quiz_game.py
+++ quiz_game.py
@@ -6,27 +6,33 @@ if playing.lower() != "yes":    # to lowercase all the letters
     quit()
 
 print("Okay! Let's Play")
-
+score = 0
 answer = input("What does CPU stand for? ").lower()
 if answer == "central processing unit":
     print("correct")
+    score += 1
 else:
     print("Wrong")
 
 answer = input("What does GPU stand for? ").lower()
 if answer == "graphics processing unit":
     print("correct")
+    score += 1
 else:
     print("Wrong")
 
 answer = input("What does RAM stand for? ").lower()
 if answer == "random access memory":
     print("correct")
+    score += 1
 else:
     print("Wrong")
 
 answer = input("What does PSU stand for? ").lower()
 if answer == "power supply unit":
     print("correct")
+    score += 1
 else:
     print("Wrong")
+
+print("\nYou got "+ str(score) + " questions correct!")
